a3a404cafdd79fed2d3586de662b7a7b63440202
antoniocl11 <antcablin1@alum.us.es>
2020-03-27 12:52:56 +0100

    html-actualizado

--- IISSI2-MIDDLE_EARTH-2020/index.php
+++ IISSI2-MIDDLE_EARTH-2020/index.php
@@ -1,3 +1,25 @@
 <?php
 echo "Esta ser&aacute; la p&aacute;gina principal de la web MIDDLE-EARTH";
 ?>
+
+<!DOCTYPE html>
+<html lang = "es">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <img src="images/dinosaur.jpg">
+    </head>
+        <body>
+            <div class="topnav" id = "menu">
+			    <a class="active" href="inicio.php">Inicio</a>
+			    <a href="empleados.php">Empleados</a>
+			    <a href="clientes.php">Clientes</a>
+			    <a href="pedidos.php">Pedidos</a>
+			    <a href="ventas.php">Ventas</a>
+			    <a href="facturas.php">Facturas</a>	
+			    <a href="proveedores.php">Proveedores</a>
+			    <a href="productos.php">Productos</a>
+			    <a href="transportes.php">Transportes</a>
+		    </div>
+    </body>
+</html>
