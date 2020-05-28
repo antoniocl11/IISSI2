72e6cb18ab19519ed46f616ea5a52760122de427
Beelthazad <eduardocgcadiz@gmail.com>
2020-05-28 15:13:58 +0200

    nuevo estilo productos

--- IISSI2-MIDDLE_EARTH-2020/productos_principal.php
+++ IISSI2-MIDDLE_EARTH-2020/productos_principal.php
@@ -17,21 +17,29 @@
     <link type="text/css" href="css/productos.css" rel="stylesheet">
 </head>
 <div id="carrusel">
-            <h4 class="titulo_reservas">Elige un producto para reservar</h4>
+            <h4 class="titulo_reservas">Elige un producto para reservar...</h4>
             <div class="contenedor1">
                 <div class="productos">
+                    <h5 class="titprod">Comic Marvel: The Incredible Hulk</h5>
                     <a href="form_alta_reserva.php"><img class="imagenes" src="images/s0.jpg" ></img></a>
-                    <h5>Comic Marvel: The Incredible Hulk</h5>
+                    <p class="preciores">10,00€ </p>
+                    <p><button class="breserva">Reservar</button></p>
                 </div>
                 <div class="productos">
+                    <h5 class="titprod">Juego de Mesa: El señor de los anillos</h5>
                     <a href="form_alta_reserva.php"><img class="imagenes" src="images/s1.jpg" ></img></a>
-                    <h5>Juego de Mesa: El señor de los anillos</h5>
+                    <p class="preciores">10,00€</p>
+                    <p><button class="breserva">Reservar</button></p>          
                 </div><div class="productos">
+                    <h5 class="titprod">Camiseta Marvel Oficial</h5>
                     <a href="form_alta_reserva.php"><img class="imagenes" src="images/s2.jpg" ></img></a>
-                    <h5>Camiseta Marvel Oficial</h5>
+                    <p class="preciores">10,00€ </p>
+                    <p><button class="breserva">Reservar</button></p>
                 </div><div class="productos">
+                    <h5 class="titprod">Figura SpiderMan Traje Avanzado</h5>
                     <a href="form_alta_reserva.php"><img class="imagenes" src="images/s3.jpg"></img></a>
-                    <h5>Figura SpiderMan Traje Avanzado</h5>
+                    <p class="preciores">10,00€</p>
+                    <p><button class="breserva">Reservar</button></p>
                 </div>
 </div>
 </html>
