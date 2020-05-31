ab69bc06843a558a4758263514746ce8a83d73e9
antoniocl11 <antcablin1@alum.us.es>
2020-05-31 20:09:34 +0200

    arreglado accion alta reserva

--- IISSI2-MIDDLE_EARTH-2020/accion_alta_reserva.php
+++ IISSI2-MIDDLE_EARTH-2020/accion_alta_reserva.php
@@ -34,15 +34,15 @@
         if(añadir_reserva($conexion, $nuevaReserva)==""){
       ?>
             <div id="div_exito">
-              <h1>Reserva <?php echo $nuevaReserva["nombre"]; ?>, añadido con éxito</h1>
+              <h1>Reserva <?php echo $nuevaReserva["producto"]; ?>, añadida con éxito</h1>
               <div id="div_volver">	
-                Pulsa <a href="index_dos.php">aquí</a> para ir a la página principal.
+                Pulsa <a href="productos_principal.php">aquí</a> para ir a los productos.
               </div>
             </div>
         <?php } 
         
         else { ?>
-                  <h2>La reserva ya existe en la base de datos.</h2>
+                  <h2>La reserva ya se ha realizado.</h2>
                   
               <div>
                 Pulsa <a href="form_alta_reserva.php"> aquí</a> para volver al formulario o pulsa <a href="index_dos.php">aquí</a> para ir a la página principal
