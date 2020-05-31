df0cb2870c872f06cca0d0cac4e19740c8b02f90
Beelthazad <eduardocgcadiz@gmail.com>
2020-05-31 21:03:59 +0200

    Mostrar_reservasuser.php arreglado Arreglado el link a productos_dinamico.php que faltaba

--- IISSI2-MIDDLE_EARTH-2020/mostrar_reservasuser.php
+++ IISSI2-MIDDLE_EARTH-2020/mostrar_reservasuser.php
@@ -9,22 +9,19 @@ if (!isset($_SESSION['loggedin'])) {
 	exit;
 } else {
 	try {
-		$stmt = $conexion->prepare('SELECT PRODUCTO.Nombre, PRODUCTO.Precio, Reserva.OID_U,  Reserva.FECHA, Reserva.PRODUCTO, Reserva.EMAIL, Reserva.NOMBRE FROM Producto INNER JOIN Reserva ON Reserva.OID_U = :nombreuser');
-		$stmt->bindParam(':nombreuser', $_SESSION['oid_u']);
+		$stmt = $conexion->prepare('SELECT FECHA, PRODUCTO, EMAIL FROM RESERVA WHERE EMAIL = :correouser');
+		$stmt->bindParam(':correouser', $_SESSION['name']);
 		$stmt->execute();
 
 		$stmt->bindColumn('FECHA', $fecha);
 		$stmt->bindColumn('PRODUCTO', $producto);
 		$stmt->bindColumn('EMAIL', $email);
-		$stmt->bindColumn('NOMBRE', $nombre);
-		
-		
-		if (!isset($producto)) {
+
+		if (!isset($stmt)) {
 			echo '&#9888;&nbspNinguna reserva... de momento.&nbsp&#9888;';
 		} else {
 			while ($row = $stmt->fetch(PDO::FETCH_BOUND)) {
-				echo '<b>&#9733;&nbspProducto:&nbsp</b>'. $producto .'<b>-- Fecha:&nbsp</b>'. $fecha . '<b>-- Email:&nbsp</b>'. $email . '<b>-- Nombre:&nbsp</b>'. $nombre ;
-				echo 'Todo va bien!';
+				echo '<b>&#9733;&nbsp&nbsp</b>'. $producto .'<b>&nbsp&nbsp  &#x1f4c5;&nbsp&nbsp&nbsp</b>'. $fecha . '<br />';
 				}
 		}
 	} catch ( PDOException $e ){
